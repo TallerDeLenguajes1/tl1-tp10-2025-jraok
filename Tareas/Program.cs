efa506d80772f8574e4a63f223a6420208447289
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-29 01:08:39 -0300

    primeras lineas con la solicitud a la api y deserializacion del archivo

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using EspacioTarea;
+using System.Text.Json;
+using System.Net.Http;
+// Cliente para realizar la solicitud HTTP
+HttpClient cliente = new HttpClient();
+// URL del recurso JSON (tareas)
+string url = "https://jsonplaceholder.typicode.com/todos";
+// Instancia de HttpClient para conectarse a la API REST de tareas
+HttpResponseMessage respuesta = await cliente.GetAsync(url);    // Solicitud GET a la API
+respuesta.EnsureSuccessStatusCode();    // Lanza excepción si el estado no es exitoso
+// Lee el contenido de la respuesta como string JSON
+string jsonRespuesta = await respuesta.Content.ReadAsStringAsync();
+// Lista con objetos deserializados del JSON
+List<Tarea> listaTareas = JsonSerializer.Deserialize<List<Tarea>>(jsonRespuesta);
