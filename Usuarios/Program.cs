56c656b3b9a490aed4916d44bcad24c9d87b092b
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-29 14:18:27 -0300

    impresion de los primeros 5 usuarios, serializacion y guardado del archivo JSON

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -12,7 +12,19 @@ try
     Respuesta.EnsureSuccessStatusCode();
     string JsonRespuesta = await Respuesta.Content.ReadAsStringAsync();
     List<Usuario> Usuarios = JsonSerializer.Deserialize<List<Usuario>>(JsonRespuesta);
-    
+    if (Usuarios is not null)
+    {
+        Console.WriteLine("\n\t\t---USUARIOS---");
+        Console.WriteLine($"\n| {"NOMBRE",-27} | {"CORREO",-25} | {"DIRECCIÓN",-50} |");
+        Console.WriteLine(new string('-', 105));
+        for (int i = 0; i < 5; i++)
+        {
+            Usuario U = Usuarios[i];
+            Console.WriteLine($"| {U.Nombre,-27} | {U.Correo,-25} | {U.Direccion.Ciudad}, {U.Direccion.Calle} , {U.Direccion.Unidad}");
+        }
+        File.WriteAllText("Usuarios.json", JsonSerializer.Serialize(Usuarios, new JsonSerializerOptions { WriteIndented = true }));
+        Console.WriteLine("\nArchivo 'usuarios.json' guardado correctamente.");
+    }
 }
 catch (Exception error){
     Console.WriteLine("ERROR: " + error.Message);
