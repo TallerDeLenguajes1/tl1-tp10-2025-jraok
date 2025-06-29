6c04a4a98e8887b91fd34c6361a89c6c59f7bb62
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-29 19:47:29 -0300

    solicitud Get a la api realizada

--- MiWebApi/Program.cs
+++ MiWebApi/Program.cs
@@ -1,5 +1,17 @@
 ﻿using EspacioUniversidad;
 using System.Text.Json;
 using System.Net.Http;
-
+using Internal;
+// Cliente para realizar la solicitud
 HttpClient Cliente = new HttpClient();
+// Api con la lista de universidades en Reino Unido
+string Api = "http://universities.hipolabs.com/search?country=United+Kingdom";
+
+try
+{
+    HttpResponseMessage Respuesta = await Cliente.GetAsync(Api);
+}
+catch (Exception error)
+{
+    Console.WriteLine($"\n\t\t---ERROR: {error.Message}");
+}
